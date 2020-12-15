bf81bff887cb98c23a5515157c52fb832dd44151
Mai-Trinh Huynh-Ha <hhmtrinh@apcs.vn>
2020-12-15 11:37:09 +0700

    comment for chinese remainder theorem

--- chinese_remainder_7.py
+++ chinese_remainder_7.py
@@ -1,4 +1,3 @@
-
 def findU(z, n):
     u = 0
     while (True):
@@ -6,8 +5,21 @@ def findU(z, n):
             return u
         u += 1 
 
-def findX(N, A):
-    production = 1
+
+#N = [n_1, n_2, n_3, ...], A = [a_1, a_2, a_3, ...]
+# findX(N, A) is the solution of the system 
+# x = a_1 (mod n_1)
+# x = a_2 (mod n_2)
+# x = a_3 (mod n_3)
+# ...
+
+# Example:
+# N = [27, 20]
+# A = [15, 16]
+# print(findX(N, A))
+
+def findX(N, A): 
+    production = 1 # production = n_1 . n_2 . n_3 . ...
     y = 0
     for n in N:
         production *= n  
@@ -16,7 +28,3 @@ def findX(N, A):
         y += A[i] * _n * findU(_n, N[i])
     return y % production
 
-
-N = [27, 20]
-A = [15, 16]
-print(findX(N, A))
